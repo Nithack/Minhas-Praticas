a3347bfe38ac1d62bd33eca9a1098f31e55db0b0
Andrey Nithack <andreynithack@gmail.com>
2021-03-30 21:33:27 -0300

    Facebook Clone and Resilia10

---
+++ AulasResilia/Aula10/js/script.js
@@ -0,0 +1,22 @@
+// var x = "andrey"
+
+// var y = "29"
+
+// console.log("Meu nome é: " + x + " com a idade de: " + y)
+
+var cliente = "Alcides Garcia"
+var conta = 67768734
+var saldo = 322.95
+var deposito = 5
+console.log("Cliente: " + cliente)
+console.log("Conta: " + conta)
+console.log("Saldo: " + saldo)
+
+saldo += deposito
+
+console.log("Novo deposito na conta de : " + cliente +  " Saldo atualizado: " + saldo)
+
+console.log(typeof(cliente))
+console.log(typeof(conta))
+console.log(typeof(saldo))
+console.log(typeof(deposito))
