6030a1725d66d514a9f078d4f6d32558cae1d0fd
Andrey Nithack <andreynithack@gmail.com>
2021-04-01 11:38:35 -0300

    Aula12 Resilia

---
+++ AulasResilia/Aula12/js/script.js
@@ -0,0 +1,28 @@
+var nomeAlimento = prompt("Qual Alimento").toLowerCase();
+var valorSemanaAnterior = 0
+var valorSemanaAtual = 0
+
+if (nomeAlimento == "tomate" || nomeAlimento == "laranja" || nomeAlimento == "abacaxi" || nomeAlimento == "limão") {
+    valorSemanaAnterior = prompt("Valor da semana Anterior")
+    if(valorSemanaAnterior >= 0){
+        valorSemanaAtual = prompt("Valor da semana Atual")
+        if(valorSemanaAtual >= 0){
+            if(valorSemanaAtual > valorSemanaAnterior){
+                alert('O produto: ' + nomeAlimento + ' aumentou em: R$' + (valorSemanaAtual - valorSemanaAnterior)) 
+            }else if( valorSemanaAtual < valorSemanaAnterior){
+                alert('O produto: ' + nomeAlimento + ' diminuiu em: R$' + (valorSemanaAnterior - valorSemanaAtual))
+            }else{
+                alert('O produto: ' + nomeAlimento + ' manteve o mesmo valor')
+            }
+        }else{
+            alert("Valor invalido")
+        }
+    }else{
+        alert("Valor invalido")
+    }
+
+} else {
+    alert("Produto "+ nomeAlimento +  "não está na lista ")
+}
+
+location.reload();
