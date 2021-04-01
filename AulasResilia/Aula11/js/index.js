354ef4bd8cad7c0f8050d157fceb8b7fa5ec67c2
Andrey Nithack <andreynithack@gmail.com>
2021-04-01 00:33:55 -0300

    aula11

---
+++ AulasResilia/Aula11/js/index.js
@@ -0,0 +1,7 @@
+var comeuTudo = 'Sim'
+
+if (comeuTudo == "Sim"){
+    console.log('Sobremesa liberada')
+}else{
+    console.log("Sem Sobremesa!")
+}
---
+++ AulasResilia/Aula11/js/termometro.js
@@ -0,0 +1,19 @@
+var temperatura = 99
+
+if (temperatura > 38) {
+    console.log('Você está com febre')
+
+} else if (temperatura < 35) {
+    console.log('Você está com Hipotermia')
+} else {
+    console.log('Você está Bem')
+}
+
+var comeuTudo = 'sim'
+
+if (comeuTudo.toUpperCase() == 'SIM') {
+    console.log('Voce comeu tudo')
+
+} else{
+    console.log('Voce nao comeu tudo')
+}
