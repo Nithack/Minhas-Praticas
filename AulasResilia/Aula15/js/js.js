ac0052423ffac3306d5a97682f1714d5886f3f92
Andrey Nithack <andreynithack@gmail.com>
2021-04-06 13:35:59 -0300

    Update Pizza Resilia 15

--- AulasResilia/Aula15/js/js.js
+++ AulasResilia/Aula15/js/js.js
@@ -1,51 +1,63 @@
 // alert(" 1: Atum: R$20,00 /  2: Calabresa : R$22,50 /  3: Portuguesa : R$34,00 / 4: Musarela : R$20,00 / 5: Frango com catupiry  : R$35,00 / 6: Marguerita : R$20,00")
 
-var sabor = prompt("Qual o sabor da pizza ///  1: Atum: R$20,00 /  2: Calabresa : R$22,50 /  3: Portuguesa : R$34,00 / 4: Musarela : R$20,00 / 5: Frango com catupiry  : R$35,00 / 6: Marguerita : R$20,00")
-var valorDinheiro = parseInt(prompt("Dinheiro"))
+var sabor = parseInt(prompt("Qual o sabor da pizza ///  1: Atum: R$20,00 /  2: Calabresa : R$22,50 /  3: Portuguesa : R$34,00 / 4: Musarela : R$20,00 / 5: Frango com catupiry  : R$35,00 / 6: Marguerita : R$20,00"))
 
 
-var cardapio = (sabor, valorDinheiro) => {
 
-    if (1) {
-        return ()=>{
-            alert("A pizza escolhida é de Atum, no valor de R$20,00, " + calcularTroco(valorDinheiro, 20.00))
-        }
-    } else if (2) {
-        return ()=>{
-            alert("A pizza escolhida é de Calabresa, no valor de R$22,50, " + calcularTroco(valorDinheiro, 22.50))
-        }
+function cardapio (sabor){
+    if (sabor == 1) {
+        let valorPizza = 20.00
+        let nomePizza = "Atum"
+        return alertTela(nomePizza, valorPizza)
 
-    } else if (3) {
-        return ()=>{
-            alert("A pizza escolhida é de Portuguesa, no valor de R$20,00, " + calcularTroco(valorDinheiro, 34.00))
-        }
+    } else if (sabor ==2) {
+        let valorPizza = 22.50
+        let nomePizza = "Calabresa"
+        return alertTela(nomePizza, valorPizza)
 
-    } else if (4) {
-        return ()=>{
-            alert("A pizza escolhida é de Musarela, no valor de R$20,00, " + calcularTroco(valorDinheiro, 20.00))
-        }
+    } else if (sabor ==3) {
+        let valorPizza = 34.00
+        let nomePizza = "Portuguesa"
+        return alertTela(nomePizza, valorPizza)
 
-    } else if (5) {
-        return ()=>{
-            alert("A pizza escolhida é de Frango com catupiry, no valor de R$35,00, " + calcularTroco(valorDinheiro, 35.00))
-        }
+    } else if (sabor ==4) {
+        let valorPizza = 20.00
+        let nomePizza = "Musarela"
+        return alertTela(nomePizza, valorPizza)
 
+    } else if (sabor ==5) {
+        let valorPizza = 35.00
+        let nomePizza = "Frango Com Caturpiry"
+        return alertTela(nomePizza, valorPizza)
+    } else if (sabor ==6) {
+        let valorPizza = 20.00
+        let nomePizza = "Marguerita"
+       return alertTela(nomePizza, valorPizza)
     } else {
-        return ()=>{
-            alert("A pizza escolhida é de Marguerita, no valor de R$20,00, " + calcularTroco(valorDinheiro, 20.00))
+        return () => {
+            alert('Pizza não encontrada')
         }
-
     }
-
-
 }
 
 function calcularTroco(valorDinheiro, valorPizza) {
-    if(valorDinheiro > valorPizza){
-        return `O Troco sera de  ${valorDinheiro  - valorPizza}`
-    }else{
+    if (valorDinheiro > valorPizza) {
+        return `O Troco sera de  ${valorDinheiro - valorPizza}`
+    } else {
         return `Ǹão sera necessário troco`
     }
 }
-
-cardapio(sabor, valorDinheiro)()
+function alertTela(nomePizza, valorPizza) {
+    var valorDinheiro = parseInt(prompt("Dinheiro"))
+    if(valorDinheiro >= valorPizza){
+        return () => {
+            alert(`A pizza escolhida é de ${nomePizza}, no valor de ${valorPizza}, ${calcularTroco(valorDinheiro, valorPizza)} `)
+        }
+    }else {
+        return ()=>{
+            alert("Valor abaixo do valor da pizza")
+        }
+    }
+    
+}
+cardapio(sabor)()
